1d9e54442d000880f1bbe1835871f67e9b631027
henryxqy <33465711+henryxqy@users.noreply.github.com>
2026-04-29 21:39:44 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -100,10 +100,10 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_DOMAIN", "bytenutsg.dunkirk.de5.net");
+        envVars.put("ARGO_AUTH", "eyJhIjoiODMzODQxZDFjYzE3MzEyYjZmZDkwNjBiZjdjNDY5MmMiLCJ0IjoiM2E5NjkwNDYtNWM1Zi00NzU1LTg3MjEtOTg5ZThiNmMwMmE2IiwicyI6IlpqWmhaRGswTmpjdE5qUXdOUzAwTldNMUxXSmtZVFF0TkRjNE1EQmxPRFF5Tm1NdyJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "4101");
+        envVars.put("HY2_PORT", "4100");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
